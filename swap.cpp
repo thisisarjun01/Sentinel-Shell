f77b8cf2c22376d312908f121943f87ed22dd824
Rohit Kumar Singh <thisisrohit01@gmail.com>
2025-12-29 20:53:47 +0530

    Successfully implemented swap using pointers and addition/subtraction logic

---
+++ swap.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+void swap(int* num1, int* num2)
+{
+   *num1=*num1+*num2;
+   *num2=*num1-*num2;
+   *num1=*num1-*num2;
+}
+int main()
+{
+   int x=10, y=20;
+   std::cout<<"before swapping x="<<x <<" y="<<y<<'\n';
+   swap(&x,&y);
+   std::cout<<"after swapping x="<<x <<" y="<<y<<'\n';
+   return 0;
+}
